c2746517b1e5449e991a845e94589c919d2edd9a
Nathan Chan <e334cn@gmail.com>
2015-10-24 14:43:10 -0700

    Cleanup

--- MedicalResearch/LEGraph.r
+++
@@ -1,9 +0,0 @@
-require(zoo)
-
-LEGraph <- function() {
-	le <- rev(read.table("life expectancy")[,1])
-	delta <- (le[2:length(le)] - le[1:length(le)-1]) / le[1:length(le)-1]
-	x = 1930:2006
-	data <- data.frame(years = x, lifeExpectancy = le[2:length(le)], delta = delta)
-	plot(data$years, data$delta)
-}
